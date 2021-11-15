f146c647bd24e6080c16ba3d2e964e72f28f4712
Mirran64 <zone64@hotmail.it>
2021-11-15 17:15:59 +0100

    second commit

---
+++ jscript.js
@@ -0,0 +1,10 @@
+/* var divisori = document.getElementById("lineeDivisorie");
+var gDivisori = divisori.getContext("2d");
+
+gDivisori.stokeStyle = "Black";
+gDivisori.lineWidth = 10;
+gDivisori.moveTo(0, 20);
+gDivisori.lineTo(320, 20);
+gDivisori.moveTo(0,160);
+gDivisori.lineTo(320,160);
+gDivisori.stroke(); */
